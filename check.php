ed615512d0d3c9b2578cb931624113d3f3ce1691
D1114182041 <D1114182041@gm.lhu.edu.tw>
2024-10-04 15:49:50 +0800

    增加新頁面+表格

---
+++ check.php
@@ -0,0 +1,7 @@
+<?php
+     $account = $_GET['account'];
+     $password = $_GET['password'];
+     $check_code = $_GET['code'];
+     
+     echo ' 帳號: ' . $account . ', 密碼: ' . $password . ', 驗證碼: ' . $check_code  ; 
+?>
---
+++ tablle_gen.php
@@ -0,0 +1,6 @@
+<?php
+     $row_count = $_GET['row_num'];
+     $col_count = $_GET['col_num'];
+     
+     echo ' 表格大小: ' . $row_count . ', 列: ' . $col_count . ', 行: '; 
+?>
