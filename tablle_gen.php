14e1c3ca25fcb9d33fb3d515e95c776acc80b7c1
D1114182041 <D1114182041@gm.lhu.edu.tw>
2024-10-08 12:19:40 +0800

    加表格

--- tablle_gen.php
+++ tablle_gen.php
@@ -1,6 +1,24 @@
 <?php
      $row_count = $_GET['row_num'];
      $col_count = $_GET['col_num'];
-     
+     $gender = $_GET['gender'];
+     $dept = $_GET['dept'];
+     $message = $_GET['message'];
+
      echo ' 表格大小: ' . $row_count . ', 列: ' . $col_count . ', 行: '; 
+     echo '性別' . $gender ."。";
+     echo '科系' . $dept ."。";
+     echo '留言' . $message ."。";
+
+     echo "<table border=\"1\">";
+
+     for ($i = 0; $i < $row_count; $i++) {
+          echo '<tr>';
+
+          for ($j = 0; $j < $col_count; $j++) {
+               echo '<td>行'. ($i+1) . '列' . ($j+1) . '</td>';
+          }
+          echo '</tr>';
+     }
+     echo '</table>';
 ?>
